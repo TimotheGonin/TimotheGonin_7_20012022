318b5ded545ccc34849051e982070f5294a9db24
TimotheGonin <gonin.timothe@gmail.com>
2022-01-26 10:58:49 +0100

    :construction: test: add function to switch between button or input

--- js/app.js
+++ js/app.js
@@ -1,16 +1,35 @@
 /**
  * BUTTONS CHEVRON
  */
-const button = document.querySelector("nav [role='button']");
-const icon = document.querySelector(".icon__chevron");
+// const button = document.querySelector("nav [role='button']");
+// const icon = document.querySelector(".icon__chevron");
 
-button.addEventListener('click', chevronTurn)
+// button.addEventListener('click', chevronTurn)
 
-function chevronTurn(){
-  if(icon.className === 'icon__chevron'){
-    icon.classList.add('icon__chevron--up');
-  }
-  else {
-    icon.classList.remove('icon__chevron--up');
+// function chevronTurn(){
+//   if(icon.className === 'icon__chevron'){
+//     icon.classList.add('icon__chevron--up');
+//   }
+//   else {
+//     icon.classList.remove('icon__chevron--up');
+//   }
+// }
+const buttonContainer = document.querySelector('#buttonIngredients');
+const inputContainer = document.querySelector('#inputIngredients');
+
+let buttonStatus = true;
+
+function componentSwicth(){
+  if(buttonStatus){
+    inputContainer.style.display = 'block';
+    buttonContainer.style.display = 'none';
+    buttonStatus = false;
+  } else if(!buttonStatus) {
+    inputContainer.style.display = 'none';
+    buttonContainer.style.display = 'block';
+    buttonStatus = true;
   }
 }
+
+buttonContainer.addEventListener('click', componentSwicth)
+inputContainer.addEventListener('click', componentSwicth)

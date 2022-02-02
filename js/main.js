a061ebe8271ea4eb0e2e6cf4f716ed7da349e528
TimotheGonin <gonin.timothe@gmail.com>
2022-02-02 16:54:59 +0100

    test: get ingrdient list (filtered)

--- js/main.js
+++ js/main.js
@@ -1,6 +1,18 @@
 import recipes from "../data/recipes.js";
 
-for (let i = 0; i < recipes.length; i++) {
-  console.log(recipes[i]);
-  
+
+
+function getIngredients(data){
+  let ingredientsList = []
+
+  for (let i = 0; i < data.length; i++) {
+    for (let i2 = 0; i2 < data.length; i2++) {
+      ingredientsList.push(data[i2].ingredients[1].ingredient);
+    }
+  }
+
+  let ingredientListClean = [... new Set(ingredientsList)];
+  return ingredientListClean;
 }
+
+console.log(getIngredients(recipes));

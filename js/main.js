ee440e6b23c5d08f82d1bf8facbd552dc70d91d7
TimotheGonin <gonin.timothe@gmail.com>
2022-02-02 17:00:55 +0100

    refactor: format recipes.js

--- js/main.js
+++ js/main.js
@@ -15,4 +15,18 @@ function getIngredients(data){
   return ingredientListClean;
 }
 
+function getAppliances(data){
+  let applicancesList = []
+
+  for (let i = 0; i < data.length; i++) {
+    for (let i2 = 0; i2 < data.length; i2++) {
+      applicancesList.push(data[i2].appliance);
+    }
+  }
+
+  let applicancesListClean = [... new Set(applicancesList)];
+  return applicancesListClean;
+}
+
 console.log(getIngredients(recipes));
+console.log(getAppliances(recipes));

6ef46409251b2d75bb6b4e4b5110b93371488040
TimotheGonin <gonin.timothe@gmail.com>
2022-02-03 18:00:26 +0100

    :recycle: refactor ingredients list without  duplicates

--- js/main.js
+++ js/main.js
@@ -3,30 +3,32 @@ import recipes from "../data/recipes.js";
 
 
 function getIngredients(data){
-  let ingredientsList = []
+  const allIngredients = new Array;
 
-  for (let i = 0; i < data.length; i++) {
-    for (let i2 = 0; i2 < data.length; i2++) {
-      ingredientsList.push(data[i2].ingredients[1].ingredient);
-    }
+  for(let i = 0; i < data.length; i++){
+    const ingredientsList = data[i].ingredients;
+    ingredientsList.forEach(ingredient => {
+      const ingredientName = ingredient.ingredient;
+      allIngredients.push(ingredientName.toLowerCase());
+    });
   }
 
-  let ingredientListClean = [... new Set(ingredientsList)];
-  return ingredientListClean;
+  const allIngredientsNoDuplicates = new Set(allIngredients);
+  return allIngredientsNoDuplicates;
 }
 
-function getAppliances(data){
-  let applicancesList = []
+// function getAppliances(data){
+//   let applicancesList = []
 
-  for (let i = 0; i < data.length; i++) {
-    for (let i2 = 0; i2 < data.length; i2++) {
-      applicancesList.push(data[i2].appliance);
-    }
-  }
+//   for (let i = 0; i < data.length; i++) {
+//     for (let i2 = 0; i2 < data.length; i2++) {
+//       applicancesList.push(data[i2].appliance);
+//     }
+//   }
 
-  let applicancesListClean = [... new Set(applicancesList)];
-  return applicancesListClean;
-}
+//   let applicancesListClean = [... new Set(applicancesList)];
+//   return applicancesListClean;
+// }
 
 console.log(getIngredients(recipes));
-console.log(getAppliances(recipes));
+// console.log(getAppliances(recipes));

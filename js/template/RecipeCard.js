eb23951521491d099a9975a7f6bc0c8443bf0fa1
TimotheGonin <gonin.timothe@gmail.com>
2022-02-07 16:30:08 +0100

    :construction: add "cardHeader module"

--- js/template/RecipeCard.js
+++ js/template/RecipeCard.js
@@ -14,20 +14,27 @@ class RecipeCard{
     const recipeCard = document.createElement('div');
     recipeCard.classList.add('col-4');
 
+    //Card header CREATION
+    const cardHeader = document.createElement('div');
+    cardHeader.className = "card-header border-0";
+    cardHeader.innerHTML = `
+    <div class="card-header border-0">
+      <div class="row">
+        <div class="col-8 d-flex align-items-center">
+          <h2>${this._data.name}</h2>
+        </div>
+        <div class="col-4 d-flex align-items-center justify-content-evenly">
+          <span class="icon--clock"></span>
+          <span class="fw-bold">${this._data.time} min</span>
+        </div>
+      </div>
+    </div>
+    `;
+
+    //Card CREATION
     recipeCard.innerHTML = `
     <div class="card bg-light">
       <div class="card-img-top"></div>
-      <div class="card-header border-0">
-        <div class="row">
-          <div class="col-8 d-flex align-items-center">
-            <h2>${this._data.name}</h2>
-          </div>
-          <div class="col-4 d-flex align-items-center justify-content-evenly">
-            <span class="icon--clock"></span>
-            <span class="fw-bold">${this._data.time} min</span>
-          </div>
-        </div>
-      </div>
       <div class="card-body">
         <div class="row">
           <div class="col-5 p">
@@ -67,6 +74,12 @@ class RecipeCard{
       </div>
     </div>
     `;
+
+    const cardContainer = recipeCard.childNodes[1];
+    const cardImage = cardContainer.childNodes[1];
+    
+    cardImage.insertAdjacentElement('afterEnd', cardHeader);
+
 
     return recipeCard
   }

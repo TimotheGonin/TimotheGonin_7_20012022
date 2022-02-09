9e190e9b76a743d150c984830de6a10b59e19a2f
TimotheGonin <gonin.timothe@gmail.com>
2022-02-09 14:02:32 +0100

    :construction: add content button/input

--- js/template/FilterButton.js
+++ js/template/FilterButton.js
@@ -31,9 +31,25 @@ function filterButtonCREATOR(type){
 const filterButton = document.createElement('div');
 filterButton.className = "col-2 p-0 me-3";
 filterButton.id = "buttonIngredients";
-console.log(filterButton);
+
+filterButton.innerHTML = `
+  <div class="button-filter btn btn-primary p-4" role="button">
+    <h2 class="fs-5 m-0 text-white">Ingédients</h2>
+    <span class="icon__chevron"></span>
+  </div>
+`;
+
 
 const filterInput = document.createElement('div');
 filterInput.className = "col p-0 me-3 rounded";
 filterInput.id = "inputIngredients";
+
+filterInput.innerHTML = `
+  <form class="button-filter bg-primary p-4 rounded-0 rounded-top">
+    <input type="text" class="button-filter__input" placeholder="Rechercher un ingédient" aria-label="Rechercher un ingédient">
+    <span class="icon__chevron icon__chevron--up"></span>
+  </form>
+`;
+
+console.log(filterButton);
 console.log(filterInput);

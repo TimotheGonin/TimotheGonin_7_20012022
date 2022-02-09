20b39e71229b2e8e2a8bb31aef45572b0e896938
TimotheGonin <gonin.timothe@gmail.com>
2022-02-09 10:40:47 +0100

    :construction: sibling navbar and create buttons container

--- js/template/FilterButton.js
+++ js/template/FilterButton.js
@@ -2,3 +2,28 @@ import { getIngredients } from "../main.js";
 import recipes from "../../data/recipes.js";
 
 console.log(getIngredients(recipes));
+
+const navBar = document.querySelector("header + nav");
+const filterButtonDeck = document.createElement('div');
+filterButtonDeck.className = "row mx-4";
+navBar.appendChild(filterButtonDeck);
+console.log(navBar);
+
+function filterButtonCREATOR(type){
+  switch (type) {
+    case 'blue':
+      
+      break;
+  
+    case 'red':
+      
+      break;
+  
+    case 'green':
+      
+      break;
+  
+    default:
+      break;
+  }
+}

7b6345cc1f0269937cdfe7237de3292fd6047a0d
TimotheGonin <gonin.timothe@gmail.com>
2022-02-11 13:56:56 +0100

    :construction: refactor: second step buttonfilter files merging

--- js/template/FilterButton.js
+++ js/template/FilterButton.js
@@ -6,17 +6,58 @@ import recipes from "../../data/recipes.js";
 const filterButtonContainer = document.querySelector("header + nav>div");
 
 
-function filterButtonCREATOR(type){
+function createfilterButton(type){
   switch (type) {
-    case 'blue':
+
+    case 'ingredients':
+      //BLOCK BUTTON
+      const ingredientsButton = document.createElement('div');
+      ingredientsButton.className = "col-2 p-0 me-3";
+      ingredientsButton.id = "buttonIngredients";
+      ingredientsButton.innerHTML = `
+        <div class="button-filter btn btn-primary p-4" role="button">
+          <h2 class="fs-5 m-0 text-white">Ingédients</h2>
+          <span class="icon__chevron"></span>
+        </div>  
+      `;  
+
+      // BLOCK FORM/INPUT
+      const ingredientsInput = document.createElement('div');
+      ingredientsInput.className = "col p-0 me-3 rounded";
+      ingredientsInput.id = "inputIngredients";
+      ingredientsInput.innerHTML = `
+        <form class="button-filter bg-primary p-4 rounded-0 rounded-top">
+          <input type="text" class="button-filter__input" placeholder="Rechercher un ingédient" aria-label="Rechercher un ingédient">
+          <span class="icon__chevron icon__chevron--up"></span>
+        </form>  
+      `;  
+
+      // DROPDOWN LIST
+      const ingredientListContainer = document.createElement('div');
+      ingredientListContainer.className = "dropDown__container container-fluid bg-primary p-3 pt-0 rounded-bottom";
+      const allIngredients = getIngredients(recipes);
+      const ingredientsList = document.createElement('ul');
+      ingredientsList.className = "dropDown__list list-unstyled list-group";
+
+      allIngredients.forEach(ingredient => {
+        const ingredientItem = document.createElement('li');
+        ingredientItem.className = "dropDown__item px-0 my-1";
+        ingredientItem.textContent = ingredient;
+        ingredientsList.appendChild(ingredientItem);
+      })  
+
+      //Append
+      ingredientListContainer.appendChild(ingredientsList);
+      ingredientsInput.appendChild(ingredientListContainer);
+      filterButtonContainer.appendChild(ingredientsButton);
+      filterButtonContainer.appendChild(ingredientsInput);
+      break;
+  
+    case 'applicances':  
       
       break;
   
-    case 'red':  
-      
-      break;
-  
-    case 'green':  
+    case 'ustensils':  
       
       break;
   
@@ -25,66 +66,15 @@ function filterButtonCREATOR(type){
   }    
 }  
 
-//BLOCK BUTTON
-const filterButton = document.createElement('div');
-filterButton.className = "col-2 p-0 me-3";
-filterButton.id = "buttonIngredients";
-
-filterButton.innerHTML = `
-  <div class="button-filter btn btn-primary p-4" role="button">
-    <h2 class="fs-5 m-0 text-white">Ingédients</h2>
-    <span class="icon__chevron"></span>
-  </div>  
-`;  
-
-// BLOCK FORM/INPUT
-const filterInput = document.createElement('div');
-filterInput.className = "col p-0 me-3 rounded";
-filterInput.id = "inputIngredients";
-
-filterInput.innerHTML = `
-  <form class="button-filter bg-primary p-4 rounded-0 rounded-top">
-    <input type="text" class="button-filter__input" placeholder="Rechercher un ingédient" aria-label="Rechercher un ingédient">
-    <span class="icon__chevron icon__chevron--up"></span>
-  </form>  
-`;  
-
-
-// BLOCK INGREDIENTS LIST (DROPDOWN)
-const ingredientsListContainer = document.createElement('div');
-ingredientsListContainer.className = "dropDown__container container-fluid bg-primary p-3 pt-0 rounded-bottom";
-const allIngredients = getIngredients(recipes);
-const ingredientsList = document.createElement('ul');
-ingredientsList.className = "dropDown__list list-unstyled list-group";
-
-allIngredients.forEach(ingredient => {
-  const ingredientItem = document.createElement('li');
-  ingredientItem.className = "dropDown__item px-0 my-1";
-  ingredientItem.textContent = ingredient;
-  ingredientsList.appendChild(ingredientItem);
-})  
-
-
-
-// ┌──────────────────────────────────────────────────────────────────────────────┐
-// │ FAMILY                                                                       │
-// └──────────────────────────────────────────────────────────────────────────────┘
-
-ingredientsListContainer.appendChild(ingredientsList);
-filterInput.appendChild(ingredientsListContainer);
-filterButtonContainer.appendChild(filterButton);
-filterButtonContainer.appendChild(filterInput);
 
 
 // ┌──────────────────────────────────────────────────────────────────────────────┐
 // │ EVENT                                                                        │
 // └──────────────────────────────────────────────────────────────────────────────┘
-const ingredientsChevron = document.querySelector('#inputIngredients .icon__chevron--up');
-
 
 let buttonStatus = true;
 
-function ingedientComponentSwicth(){
+function filterSwicth(){
   if(buttonStatus){
     filterInput.style.display = 'block';
     filterButton.style.display = 'none';
@@ -96,9 +86,10 @@ function ingedientComponentSwicth(){
   }
 }
 
+createfilterButton('ingredients')
+const filterButton = document.querySelector('#buttonIngredients');
+const filterInput = document.querySelector('#inputIngredients');
+const chevron = document.querySelector('.icon__chevron--up');
 
-const form = filterInput.childNodes[1];
-const chevron = form.childNodes[3];
-
-filterButton.addEventListener('click', ingedientComponentSwicth);
-chevron.addEventListener('click', ingedientComponentSwicth);
+filterButton.addEventListener('click', filterSwicth);
+chevron.addEventListener('click', filterSwicth);

3e1750d5feb508b486186adce2495bd9c76f7cdd
TimotheGonin <gonin.timothe@gmail.com>
2022-02-20 14:55:31 +0100

    :recycle: style: comment

--- js/main.js
+++ js/main.js
@@ -9,26 +9,32 @@ const notFoundedMessage = 'Aucune recette ne correspond à votre critère... vou
 mainSearchInput.addEventListener('input', cardsFilter);
 
 function cardsFilter(e){
+  //empty the cards Container
   cardsContainer.innerHTML = ``;
-  const entry = e.target.value.toLowerCase();
-  let entryValid = false;
 
+  const entry = e.target.value.toLowerCase();
+
+  //STATUS
+  let entryValid = false;
   entryValid = (entry.length < 3) ? entryValid = false : entryValid = true;
 
+  //ENTRY VALID
   if(!entryValid){
     console.log(entryLengthRequired);
   } else {
     console.log(lengthValidation);
 
+    //LOOP TO RECIPES
     for (let i = 0; i < recipes.length; i++) {
       const recipe = recipes[i];
       
+      //CREAT CARD
       if(recipe.name.toLowerCase().includes(entry)){
-        console.log('MATCH')
+        console.log('MATCH');
         const Template = new RecipeCard(recipe)
         cardsContainer.appendChild(
             Template.createRecipeCard()
-        )
+        );
       } else {
         console.log(notFoundedMessage);
       }

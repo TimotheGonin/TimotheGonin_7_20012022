71db72b3f56c6747057beb21abca735050380829
TimotheGonin <gonin.timothe@gmail.com>
2022-02-26 14:18:21 +0100

    🚧 test : tag removing

--- js/template/FilterButton.js
+++ js/template/FilterButton.js
@@ -224,14 +224,22 @@ Array.from(chevrons).forEach(chevron=>{
     createTagButton(this.dataset.name)
   }
 
+  function tagRemoving(){
+    this.parentNode.remove();
+  }
+
   function createTagButton(name){
     const tagButton = document.createElement('div');
+    const tagButtonClose = document.createElement('span');
     tagButton.className = 'button-tag btn btn-primary p-2 me-2';
+    tagButtonClose.className = 'icon__close';
     tagButton.setAttribute('role', 'button');
+    tagButton.setAttribute('data-active', 'true');
     tagButton.innerHTML = `
       <span class="button-tag__title fs-6 m-0 text-white">${name}</span>
-      <span class="icon__close"></span>
     `;
+    tagButton.appendChild(tagButtonClose);
+    tagButtonClose.addEventListener('click', tagRemoving);
     tagButtonsContainer.appendChild(tagButton);
   };
 
